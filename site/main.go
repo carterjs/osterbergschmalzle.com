06c9ec493db9bf71a4d5e85e145d4485115e69be
Carter Schmalzle <cjschmal00@gmail.com>
2023-02-01 17:29:20 -0700

    articles

--- site/main.go
+++ site/main.go
@@ -47,9 +47,17 @@ type (
 		Content template.HTML `json:"content"`
 	}
 	News struct {
-		Title  string `json:"title"`
-		Source string `json:"source"`
-		Link   string `json:"link"`
+		Title       string  `json:"title"`
+		ContentType string  `json:"content_type"`
+		Source      string  `json:"source"`
+		Link        string  `json:"link"`
+		Article     Article `json:"article"`
+	}
+	Article struct {
+		Slug        string        `json:"slug"`
+		Title       string        `json:"title"`
+		Description string        `json:"description"`
+		Content     template.HTML `json:"content"`
 	}
 )
 
@@ -72,7 +80,7 @@ var tmpl = template.New("").Funcs(template.FuncMap{
 						disclaimer
 					}
 				}
-			`)
+			`, nil)
 
 			disclaimerUpdated = time.Now()
 			disclaimer = data.Disclaimer
@@ -139,6 +147,34 @@ func main() {
 	http.Handle("/static/js/", minifier.Middleware(fileServer))
 	http.Handle("/static/", fileServer)
 
+	http.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		slug := strings.TrimPrefix(r.URL.Path, "/articles/")
+
+		data, err := query[struct {
+			Articles []Article `json:"articles"`
+		}](`
+			query getArticleBySlug($slug: String) {
+				articles(filter:{
+					slug: {
+						_eq: $slug
+					}
+				}) {
+					title
+					description
+					content
+				}
+			}
+		`, map[string]any{
+			"slug": slug,
+		})
+		if err != nil || len(data.Articles) == 0 {
+			render404(w)
+			return
+		}
+
+		renderPage(w, "article", data.Articles[0])
+	})
+
 	handlePage[struct {
 		Candidates []Candidate `json:"candidates"`
 	}]("/candidates", "candidates", `
@@ -171,6 +207,10 @@ func main() {
 	}]("/news", "news", `
 		{
 			news {
+				content_type
+				article {
+					slug
+				}
 				title
 				link
 				source
@@ -205,6 +245,10 @@ func main() {
 				title
 			}
 			news {
+				content_type
+				article {
+					slug
+				}
 				title
 				link
 				source
@@ -235,12 +279,11 @@ func handlePage[T any](path string, templateName string, q string) {
 		var data *T
 		if r.URL.Path != path {
 			// Not an exact match, 404
-			w.WriteHeader(http.StatusNotFound)
-			renderPage(w, "404", nil)
+			render404(w)
 			return
 		} else if q != "" {
 			var err error
-			data, err = query[T](q)
+			data, err = query[T](q, nil)
 			if err != nil {
 				log.Printf("Internal server error: %v", err)
 				http.Error(w, "failed to fetch data", http.StatusInternalServerError)
@@ -252,6 +295,11 @@ func handlePage[T any](path string, templateName string, q string) {
 	})
 }
 
+func render404(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNotFound)
+	renderPage(w, "404", nil)
+}
+
 // renderPage executes the template or returns a 500
 func renderPage(w http.ResponseWriter, templateName string, data any) {
 	err := tmpl.ExecuteTemplate(w, templateName, data)
@@ -261,10 +309,11 @@ func renderPage(w http.ResponseWriter, templateName string, data any) {
 }
 
 // query executes a graphql query against directus
-func query[T any](q string) (*T, error) {
+func query[T any](q string, variables map[string]any) (*T, error) {
 	url := directusURL + "/graphql"
 	bodyBytes, err := json.Marshal(map[string]any{
-		"query": q,
+		"query":     q,
+		"variables": variables,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal body: %w", err)
--- site/static/js/global.js
+++ site/static/js/global.js
@@ -1,8 +1,6 @@
 const header = document.getElementById("header");
 const menuToggle = document.getElementById("menuToggle")
 
-// blah blah blah
-// ////////
 menuToggle.addEventListener("click", (e) => {
     header.classList.toggle("header--menu-open")
 })
